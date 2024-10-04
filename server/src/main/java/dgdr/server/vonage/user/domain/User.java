a92e71f7d6c0eaf7529c530a67a8f8455095e60f
Jhsysng <ysng1118@naver.com>
2024-10-04 15:12:11 +0900

    feat: jpa 수정

--- server/src/main/java/dgdr/server/vonage/user/domain/User.java
+++ server/src/main/java/dgdr/server/vonage/user/domain/User.java
@@ -1,10 +1,7 @@
 package dgdr.server.vonage.user.domain;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -12,6 +9,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="users")
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String userId;
